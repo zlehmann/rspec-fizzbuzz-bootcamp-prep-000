ff0a165f970c7a4e9832b8a41028327194d802ba
zlehmann@gmail.com <zlehmann@gmail.com>
2018-07-10 02:00:14 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,11 @@
 
 def fizzbuzz(int)
   if (int % 3 == 0) && (int % 5 == 0) 
-    return "Fizz"
+    return "FizzBuzz"
   elsif int % 5 == 0 
     return "Buzz"
   elsif  int % 3 == 0
-    return "FizzBuzz"
+    return "Fizz"
   else
     return NIL
   end
