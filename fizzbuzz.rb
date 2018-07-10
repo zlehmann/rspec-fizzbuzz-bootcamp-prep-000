e403c013c919810a5cde555c45cc6241417e45cb
zlehmann@gmail.com <zlehmann@gmail.com>
2018-07-10 01:55:24 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(int)
+  if int % 3 == 0 
+    "Fizz"
+  elseif int % 5 == 0 
+    "Buzz"
+  elseif int % 3 == 0 && int % 5 == 0 
+    "FizzBuzz"
+  else
+    return NIL
+  end
+end
